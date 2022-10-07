d171337de484cc2df1cb3770ba92d9b026040877
leonard-porteria <porterialeonard@gmail.com>
2022-10-07 11:33:22 +0800

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className=' flex'>
+      <h1>SORTING ALGORITHMS </h1>
+      <div>
+        <p>algoss</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
