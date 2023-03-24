473c9e138e89088c39c99a56d4361d56e643edcf
leonard-porteria <porterialeonard@gmail.com>
2023-03-24 19:28:37 +0800

    mergesort ver1

--- src/algorithms/MergeSort.jsx
+++ src/algorithms/MergeSort.jsx
@@ -3,28 +3,80 @@ import {
   sortingFinishedAnimation,
 } from '../utils/HelperFunctions';
 
-const merge = async (left, right) => {
-  let sortedArr = []; // the sorted items will go here
-  while (left.length && right.length) {
-    // Insert the smallest item into sortedArr
-    if (left[0].value < right[0].value) {
-      sortedArr.push(left.shift());
+const mergeSortMain = async (array, start, end, setArray) => {
+  let length = array.length;
+  // BASE CASE
+  if (length <= 1) return;
+
+  let middle = Math.ceil(length / 2);
+  let leftArray = new Array(middle);
+  let rightArray = new Array(length - middle);
+
+  let i = 0; //left array
+  let j = 0; //right array
+
+  for (; i < length; i++) {
+    delayInMilliseconds();
+    if (i < middle) {
+      leftArray[i] = array[i];
     } else {
-      sortedArr.push(right.shift());
+      rightArray[j] = array[i];
+      j++;
     }
   }
-  // Use spread operators to create a new array, combining the three arrays
-  return [...sortedArr, ...left, ...right];
+
+  let mid = start + Math.ceil((end - start) / 2);
+
+  await mergeSortMain(leftArray, start, mid);
+  await mergeSortMain(rightArray, mid, end);
+  await merge(leftArray, rightArray, array, start, end, setArray);
+};
+
+const merge = async (leftArray, rightArray, array, low, high) => {
+  // VARIABLES
+  let leftSize = Math.ceil(array.length / 2);
+  let rightSize = array.length - leftSize;
+
+  let i = 0;
+  let l = 0;
+  let r = 0;
+  let a = low + i;
+
+  // SORT LEFT AND RIGHT ARRAYS INDIVIDUALLY
+  while (l < leftSize && r < rightSize) {
+    delayInMilliseconds();
+    if (leftArray[l].value < rightArray[r].value) {
+      array[i].value = leftArray[l].value;
+      i++;
+      l++;
+      a++;
+    } else if (!(leftArray[l].value < rightArray[r].value)) {
+      array[i].value = rightArray[r].value;
+      i++;
+      r++;
+      a++;
+    }
+  }
+  // MERGE LEFT ARRAY
+  while (l < leftSize) {
+    delayInMilliseconds();
+    array[i].value = leftArray[l].value;
+    i++;
+    l++;
+    a++;
+  }
+  // MERGE RIGHT ARRAY
+  while (r < rightSize) {
+    delayInMilliseconds();
+    array[i].value = rightArray[r].value;
+    i++;
+    r++;
+    a++;
+  }
 };
 
 export const MergeSort = async (array, sortingSpeed, setArray) => {
-  // Base case
-  if (array.length <= 1) return array;
-  let mid = Math.floor(array.length / 2);
-  // Recursive calls
-  let left = await MergeSort(array.slice(0, mid));
-  console.log(left);
-  let right = await MergeSort(array.slice(mid));
-  console.log(right);
-  return merge(left, right);
+  const sorted = await mergeSortMain(array, 0, array.length);
+  sortingFinishedAnimation(array, sortingSpeed, setArray);
+  console.log(sorted);
 };
--- src/utils/HelperFunctions.jsx
+++ src/utils/HelperFunctions.jsx
@@ -2,7 +2,7 @@ const randomIntFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const delayInMilliseconds = (sortingSpeed) => {
+const delayInMilliseconds = (sortingSpeed = 5) => {
   return new Promise((resolve) =>
     setTimeout(() => resolve(true), Math.abs(parseInt(sortingSpeed) - 10) * 10)
   );
